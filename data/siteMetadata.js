a28bcf472fdf15e803a761e1e044f72af81c086a
Ritu Raj <alchemyrecloak@gmail.com>
2025-10-25 04:56:43 +0530

    New Blog for collection of good blogs

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: ' Latest Blog Entries',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app', // replace this wth domain
+  siteUrl: 'https://www.bellw3ther.tech/', // replace this wth domain
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   email: 'address@yoursite.com',
   github: 'https://github.com',
