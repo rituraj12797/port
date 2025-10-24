219c16602d05fa859ca1d0fa26946cfdab5f95e0
Ritu Raj <alchemyrecloak@gmail.com>
2025-10-24 21:32:34 +0530

    new blog on aws outage

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,22 +6,20 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // {
+  //   title: 'GOD-B',
+  //   description: `A industry grade key value store implementtion in terms of performance, implemented from scratch in Go with WAL logging, Double buffering and B+ Trees. `,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
