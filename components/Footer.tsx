d3ca7b9d456f08c75857d97e3c27dd4292cac8b9
Ritu Raj <alchemyrecloak@gmail.com>
2025-10-24 14:09:17 +0530

    initials

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
