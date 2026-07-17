367952b4043c621d74e96e11233a0182987003a7
rituraj12797 <alchemyrecloak@gmail.com>
2026-07-17 03:42:59 +0000

    changes to the about section

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,20 +6,31 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  // {
-  //   title: 'GOD-B',
-  //   description: `A industry grade key value store implementtion in terms of performance, implemented from scratch in Go with WAL logging, Double buffering and B+ Trees. `,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
-  // {
-  //   title: 'The Time Machine',
-  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-  //   to the desired date and press "Go". No more worrying about lost keys or
-  //   forgotten headphones with this simple yet affordable solution.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
+  {
+    title: 'Trade Matching Engine',
+    description: `A high-performance, multi-core trade matching engine designed to replicate the core logic and architecture of a modern electronic stock exchange.`,
+    imgSrc: 'https://github.com/rituraj12797/Capitol_main/blob/main/assets/Capitol.png',
+    href: 'https://github.com/rituraj12797/Capitol_main',
+  },
+  {
+    title: 'Key Value Store',
+    description: `A key value store implementtion in Go, implemented from scratch with WAL logging, Double buffering and B+ Trees. `,
+    imgSrc: '',
+    href: 'https://github.com/rituraj12797/GoD-B',
+  },
+  {
+    title: 'Version Control System',
+    description: `A version control system in Go, implemented using Myer's diffing algorithm `,
+    imgSrc: '',
+    href: 'https://github.com/rituraj12797/VCON',
+  },
+  {
+    title: 'Mail automation app',
+    description: `A mail automation utility built on a microservice architecture that broadcasts personalized emails to large number of recipients on behalf of a user via Gmail integration using GCP`,
+    imgSrc: 'https://commu-net.vercel.app/assets/Communet-b_0tT9y4.png',
+    href: 'https://commu-net.vercel.app/',
+  }
+
 ]
 
 export default projectsData
